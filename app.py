76447cbcdeff0f7b2fa48886981f175362888579
lital <litalgr22@gmail.com>
2025-04-06 08:36:45 +0300

    Change default port from 5005 to 5001

--- app.py
+++ app.py
@@ -1677,5 +1677,5 @@ def get_build_timeline(job_name, build_number):
 
 if __name__ == '__main__':
     # Get port from environment variable or use default
-    port = int(os.environ.get('PORT', 5005))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port, debug=True)
